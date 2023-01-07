f914881872fe005d2bdb28a9ebfd3f9399ef19f7
Mateusz Tryc <mateusz.tryc0@gmail.com>
2023-01-07 18:12:07 +0100

    second commit

---
+++ src/helpers/index.js
@@ -0,0 +1,25 @@
+export const findById = (resources, id) => {
+  if(!resources) return null
+  return resources.find(r => r.id === id)
+}
+
+export const upsert = (resources, resource, parent, parentsResources) => {
+    const index = resources.findIndex(r => r.id === resource.id)
+    if(resource.id && index != -1){
+      resources[index] = resource
+    } else {
+      resources.push(resource)
+    }
+    
+    appendChildToParent(parent, parentsResources, resource.id)
+}
+
+export const appendChildToParent = (parent, parentsResources, resourceId) => {
+  if(!parent) {
+    console.warn(`Parent doesn't exist when appending child ${resourceId} to a parent`)
+    return
+  }
+  parent[parentsResources] = parent[parentsResources] || []
+  parent[parentsResources].includes(resourceId) ? '' : parent[parentsResources].push(resourceId)
+}
+
