ff0b7e3268e0cbec14517746bc146711e1a7eacb
Mateusz Grus <mateusz.tryc420@gmail.com>
2023-03-23 18:47:52 +0100

    lowered fortawesome bundle size

--- src/plugins/fontAwesome.js
+++ src/plugins/fontAwesome.js
@@ -1,5 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPencil, faCamera } from '@fortawesome/free-solid-svg-icons'
+import { faPencil } from '@fortawesome/free-solid-svg-icons/faPencil'
+import { faCamera } from '@fortawesome/free-solid-svg-icons/faCamera'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faPencil, faCamera)
 export default app => {
